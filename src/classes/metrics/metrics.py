8d33c4484be92791450c1c62a834e973c1ce397b
AloisECN <marechalalois@yahoo.com>
2025-10-29 18:22:04 +0100

    Forgot to commit each individual helper but created RV, BV, VWAP helpers. And then for intraday computed the group by minutes of the day.

--- src/classes/metrics/metrics.py
+++ src/classes/metrics/metrics.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 class MetricsCalculator :
 
@@ -18,25 +19,47 @@ class MetricsCalculator :
         self.compute_BV(df_all_days)
         self.compute_vwap(df_all_days)
 
+        #Drop unecessary columns 
+        df_all_days.drop(columns=["log_returns" , "price"], inplace= True)
+
         #daily_groups
         df_daily_groups = self.compute_intraday_profiles(df_all_days)
-        
+    
         return df_all_days , df_daily_groups
     
     #Helpers Functions
 
     def compute_RV (self, df : pd.DataFrame) -> None :
-        pass
+        #Compute log_returns 
+        df["log_returns"] = np.log(df["close"] / df["close"].shift(1) )
+        #Apply RV formula
+        rv = df.groupby("day")["log_returns"].apply(
+            lambda x: np.sqrt(np.sum(x.dropna() ** 2))
+            )
+        # Map back to each row
+        df["RV"] = df["day"].map(rv, na_action="ignore")
     
     def compute_BV (self, df : pd.DataFrame) -> None :
-        pass
+        #Log returns are already computed
+        #Compute Bipower varation
+        bv = df.groupby("day")["log_returns"].apply(
+            lambda x : (np.pi / 2) * (1 / (len(x.dropna()) - 1)) * 
+            np.sum(np.abs(x.dropna()) * np.abs(x.dropna().shift(1)))
+            )
+        # Map back to each row
+        df["BV"] = df["day"].map(bv, na_action="ignore")
     
     def compute_vwap (self, df : pd.DataFrame) -> None :
-        pass
+        #As in the notebook can aslo use close tho
+        df["price"] = (df["high"] + df["close"] + df["low"])/3
+        #Compute vwap
+        vwap = df.groupby("day")[["price" , "volume"]].apply(
+            lambda x : (np.sum(x["volume"] * x["price"] )) / np.sum(x["volume"])
+        )
+        # Map back to each row
+        df["vwap"] = df["day"].map(vwap , na_action= "ignore")
     
     def compute_intraday_profiles (self , df : pd.DataFrame) -> pd.DataFrame :
-        out = df.copy()
-        return df
     
-    
-    
+        out = df.groupby("minute_of_day").mean(numeric_only=True)
+        return out
