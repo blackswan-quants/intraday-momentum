b1c985cd0ad751c3bd402859ce2433daef2c62f7
Marechal Alois <marechalalois@yahoo.com>
2025-12-16 15:07:39 +0100

    Created the skeleton of the new backtesting engine

--- src/classes/backtest/engine_new.py
+++ src/classes/backtest/engine_new.py
@@ -1,2 +1,38 @@
-from backtesting import backtesting 
+from backtesting import Backtest , Strategy 
 
+
+"""
+Recall main idea of the strat 
+If we are outside noise area 
+    -> at the first hh.30 we buy / short
+    -> we close when either vwap becomes equal to the stock or we are back in the noise area
+    -> leverage is set dynamically at the beginning of each day 
+"""
+
+class Momentum(Strategy) :
+
+    def __init__(self):
+
+        self.data = self.data
+
+    def out_of_noise_area() :
+        pass
+    
+    def set_leverage() :
+        pass
+    
+    def close_position():
+        pass
+    
+    def next(self):
+
+        if self.outside_of_noise_area(self.data):
+            if below : 
+                self.position.short()
+            else :
+                self.position.buy()
+        
+        if self.close_position(self.data):
+            self.position.close()
+        
+        pass
